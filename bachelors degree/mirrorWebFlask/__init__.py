5808ffe1d3b29b234b4b3de5aa1428bf96a4fb7d
Andrei Sergiu Sorin Mirea <mireaandy@users.noreply.github.com>
2020-04-01 02:00:26 +0300

    updated imports in __init__.py and deleted routes.py (for code management reasons0

--- bachelors degree/mirrorWebFlask/__init__.py
+++ bachelors degree/mirrorWebFlask/__init__.py
@@ -1,8 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Bootstrap
-from flask_sqlalchemy  import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template
 from mirrorWebFlask.configuration import Config
 from mirrorWebFlask.forms import LoginForm
 
--- bachelors degree/mirrorWebFlask/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from mirrorWebFlask import app
-from mirrorWebFlask.forms import LoginForm
-
-
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', form=form)
